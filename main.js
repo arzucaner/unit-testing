c4d8320e14c4cce74ee4c0757887ce60f8f4c1e8
Arzu Caner <arzuguneycaner@gmail.com>
2023-02-01 23:49:03 +0000

    Create testing setup breakdown

---
+++ main.js
@@ -0,0 +1,29 @@
+describe('Test Example', () => {
+    it('returns true', () => {
+        expect(true).toBe(true);
+    });
+});
+
+function insertDashes(str) {
+    // write code here  
+}
+
+/**
+* Test Suite
+*/
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        // arrange
+        const value ="aba caba";
+        
+        // act
+        const result = insertDashes(value);
+
+        // log
+        console.log("result: ", result);
+
+        // assert
+        expect(result).toBe("a-b-a c-a-b-a");
+    }); 
+});
+
