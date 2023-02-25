ac0e3dee3ff741e2049e8fd32840c7783106ce3e
Arzu Caner <arzuguneycaner@gmail.com>
2023-02-25 22:48:47 +0000

    Add test suite

--- main.js
+++ main.js
@@ -27,3 +27,21 @@ describe('insertDashes()', () => {
     }); 
 });
 
+class User {
+    firstName;
+    lastName;
+    middleName;
+
+    constructor (data = {}){
+        this.firstName = data.firstName || '';
+        this.lastName = data.lastName || '';
+        this.middleName = data.middleName || '';
+
+    }
+}
+ //**
+ //* Test Suite
+
+describe(`${User.name} Class`.() => {
+   
+});
